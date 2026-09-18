7005276ad3136e07ee1157058aefa542a40ababa
porolowka <barashka197@mail.ru>
2026-09-19 00:30:12 +0700

    Update ClientInit.java

--- src/main/java/ru/supraland/ClientInit.java
+++ src/main/java/ru/supraland/ClientInit.java
@@ -2,6 +2,9 @@ package ru.supraland;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import ru.supraland.SupralandMod;
+import ru.supraland.client.SupralandNpcRenderer;
 
 public class ClientInit implements ClientModInitializer {
     @Override
