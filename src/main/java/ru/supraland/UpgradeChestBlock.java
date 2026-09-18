b4912af086df5348c5f9ab217d818ed598d32bc7
porolowka <barashka197@mail.ru>
2026-09-19 01:28:42 +0700

    Update UpgradeChestBlock.java

--- src/main/java/ru/supraland/UpgradeChestBlock.java
+++ src/main/java/ru/supraland/UpgradeChestBlock.java
@@ -1,46 +1,46 @@
-package ru.supraland;
+Run ./gradlew build
+Fetching distribution.
+Downloading https://services.gradle.org/distributions/gradle-8.7-bin.zip
+............10%.............20%.............30%.............40%............50%.............60%.............70%.............80%.............90%............100%
 
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
+Welcome to Gradle 8.7!
 
-public class UpgradeChestBlock extends BlockWithEntity {
-    protected UpgradeChestBlock(Settings settings) {
-        super(settings);
-    }
+Here are the highlights of this release:
+ - Compiling and testing with Java 22
+ - Cacheable Groovy script compilation
+ - New methods in lazy collection properties
 
-    @Nullable
+For more details see https://docs.gradle.org/8.7/release-notes.html
+
+Starting a Gradle Daemon (subsequent builds will be faster)
+
+> Configure project :
+Fabric Loom: 1.6.12
+:remapping 56 mods from modImplementation (java-api)
+
+> Task :compileJava
+/home/runner/work/supralandmod/supralandmod/src/main/java/ru/supraland/entity/SupralandNpcEntity.java:55: error: method does not override or implement a method from a supertype
     @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new UpgradeChestBlockEntity(pos, state);
-    }
-
+    ^
+/home/runner/work/supralandmod/supralandmod/src/main/java/ru/supraland/UpgradeChestBlock.java:42: error: method does not override or implement a method from a supertype
     @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
-    }
+    ^
+Note: Some input files use or override a deprecated API.
 
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            BlockEntity be = world.getBlockEntity(pos);
-            if (be instanceof UpgradeChestBlockEntity) {
-                ((UpgradeChestBlockEntity) be).open(player);
-            }
-        }
-        return ActionResult.SUCCESS;
-    }
+Note: Recompile with -Xlint:deprecation for details.
+> Task :compileJava FAILED
+2 errors
+1 actionable task: 1 executed
 
-    @Override
-    public float calcBlockBreakingDelta(BlockState state, PlayerEntity player, World world, BlockPos pos) {
-        return 0.0f;
-    }
-}
+FAILURE: Build failed with an exception.
+
+* What went wrong:
+Execution failed for task ':compileJava'.
+> Compilation failed; see the compiler error output for details.
+
+* Try:
+> Run with --info option to get more log output.
+> Run with --scan to get full insights.
+
+BUILD FAILED in 56s
+Error: Process completed with exit code 1.
