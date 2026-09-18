2ffb03af69fb7d90b3c136f501fdbebffb49b254
porolowka <barashka197@mail.ru>
2026-09-19 01:18:42 +0700

    Update SupralandNpcEntity.java

--- src/main/java/ru/supraland/entity/SupralandNpcEntity.java
+++ src/main/java/ru/supraland/entity/SupralandNpcEntity.java
@@ -1,30 +1,26 @@
 package ru.supraland.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
 
-public class SupralandNpcEntity extends PathAwareEntity {
+public class SupralandNpcEntity extends Entity {
 
     private final float[] color;
 
-    public SupralandNpcEntity(EntityType<? extends PathAwareEntity> entityType, World world, float r, float g, float b) {
-        super(entityType, world);
+    public SupralandNpcEntity(EntityType<SupralandNpcEntity> type, World world, float r, float g, float b) {
+        super(type, world);
         this.color = new float[]{r, g, b};
         this.setNoGravity(true);
         this.setInvulnerable(true);
     }
 
-    public static DefaultAttributeContainer.Builder createNpcAttributes() {
-        return PathAwareEntity.createMobAttributes()
-            .add(EntityAttributes.GENERIC_MAX_HEALTH, 1.0)
-            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.0)
-            .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0);
+    @Override
+    protected void initDataTracker() {
     }
 
     @Override
@@ -34,28 +30,16 @@ public class SupralandNpcEntity extends PathAwareEntity {
 
     @Override
     public boolean damage(DamageSource source, float amount) {
-        if (!this.world.isClient) {
-            // Звук удара мячом
-            this.world.playSound(null, this.getX(), this.getY(), this.getZ(),
-                SoundEvents.ENTITY_SLIME_ATTACK, net.minecraft.sound.SoundCategory.NEUTRAL, 1.0f, 1.5f);
-            // Вздрёг — небольшая отдача
-            this.setVelocity(0, 0.05, 0);
-            this.velocityModified = true;
+        if (!this.getWorld().isClient) {
+            this.getWorld().playSound(null, this.getX(), this.getY(), this.getZ(),
+                SoundEvents.ENTITY_SLIME_ATTACK, SoundCategory.NEUTRAL, 1.0f, 1.5f);
         }
         return false;
     }
 
     @Override
     public void tick() {
-        // Не двигается вообще, не падает
-        this.setVelocity(0, 0, 0);
-        super.tick();
-        this.setVelocity(0, 0, 0);
-    }
-
-    @Override
-    public boolean canMoveVoluntarily() {
-        return false;
+        // NPC стоит на месте
     }
 
     @Override
@@ -69,16 +53,19 @@ public class SupralandNpcEntity extends PathAwareEntity {
     }
 
     @Override
-    public void lookAt(net.minecraft.entity.Entity target, float maxYawChange, float maxPitchChange) {
-        // Не поворачивается — смотрит в одну точку
-    }
-
-    @Override
-    public void tickMovement() {
-        // Ничего не делаем — стоит на месте
+    public boolean collides() {
+        return true;
     }
 
     public float[] getColor() {
         return color;
+    }
+
+    @Override
+    protected void readCustomDataFromNbt(NbtCompound nbt) {
+    }
+
+    @Override
+    protected void writeCustomDataToNbt(NbtCompound nbt) {
     }
 }
