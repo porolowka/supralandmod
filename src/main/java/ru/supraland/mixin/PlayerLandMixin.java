5b86a29386aa1793da1a458e170d7cb1e775cf80
porolowka <barashka197@mail.ru>
2026-09-18 22:57:20 +0700

    Update PlayerLandMixin.java

--- src/main/java/ru/supraland/mixin/PlayerLandMixin.java
+++ src/main/java/ru/supraland/mixin/PlayerLandMixin.java
@@ -1,19 +1,22 @@
 package ru.supraland.mixin;
 
 import ru.supraland.PlayerUpgradeData;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
+@Mixin(Entity.class)
 public abstract class PlayerLandMixin {
 
-    // Сброс счётчика прыжков при касании земли
     @Inject(method = "onLanding", at = @At("HEAD"))
     private void onLanding(CallbackInfo ci) {
-        PlayerEntity player = (PlayerEntity) (Object) this;
+        Entity self = (Entity) (Object) this;
+        if (!(self instanceof PlayerEntity)) return;
+
+        PlayerEntity player = (PlayerEntity) self;
         if (player.getWorld().isClient) return;
 
         PlayerUpgradeData data = PlayerUpgradeData.get(player);
@@ -22,3 +25,4 @@ public abstract class PlayerLandMixin {
         }
     }
 }
+
