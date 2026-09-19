2cc30264e35301feb1e734975648fc2533b19faf
porolowka <barashka197@mail.ru>
2026-09-19 16:18:20 +0700

    Update SupralandNpcRenderer.java

--- src/main/java/ru/supraland/client/SupralandNpcRenderer.java
+++ src/main/java/ru/supraland/client/SupralandNpcRenderer.java
@@ -1,44 +1,35 @@
 package ru.supraland.client;
 
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRendererFactory.Context;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.util.math.Box;
+import ru.supraland.SupralandMod;
 import ru.supraland.entity.SupralandNpcEntity;
 
 public class SupralandNpcRenderer extends EntityRenderer<SupralandNpcEntity> {
-
-    public SupralandNpcRenderer(Context ctx) {
+    public SupralandNpcRenderer(EntityRendererFactory.Context ctx) {
         super(ctx);
     }
 
     @Override
-    public void render(SupralandNpcEntity entity, float yaw, float tickDelta,
-                       MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
-        matrices.push();
-
-        // Тело — красный/синий куб
-        float[] color = entity.getColor();
-        VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getEntitySolid(getTexture(entity)));
-
-        // Простое тело — куб 0.6 x 1.8
-        // Глаза — два белых пикселя
-        // (полная геометрия будет через модель, это заготовка)
-
-        matrices.pop();
-        super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
+    public Identifier getTexture(SupralandNpcEntity entity) {
+        return new Identifier(SupralandMod.MOD_ID, "textures/entity/red_npc.png");
     }
 
     @Override
-    public Identifier getTexture(SupralandNpcEntity entity) {
-        float[] c = entity.getColor();
-        if (c[0] > 0.5f) {
-            return new Identifier("supralandmod", "textures/entity/red_npc.png");
-        }
-        return new Identifier("supralandmod", "textures/entity/blue_npc.png");
+    public void render(SupralandNpcEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        matrices.push();
+        float[] color = entity.getColor();
+        Box box = new Box(-0.3, 0, -0.3, 0.3, 1.8, 0.3);
+        VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getLines());
+        WorldRenderer.drawBox(matrices, consumer, box, color[0], color[1], color[2], 1.0f);
+        matrices.pop();
+        super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
     }
 }
