4f4260f5b49825be311ea938ce5d14e2623ab9da
porolowka <barashka197@mail.ru>
2026-09-19 23:59:37 +0700

    Update LinkSyncClient.java

--- src/main/java/ru/supraland/LinkSyncClient.java
+++ src/main/java/ru/supraland/LinkSyncClient.java
@@ -10,35 +10,41 @@ public class LinkSyncClient {
     public static void register() {
         ClientPlayNetworking.registerGlobalReceiver(LinkSync.LINK_PACKET, (client, handler, buf, responseSender) -> {
             int action = buf.readVarInt();
-            client.execute(() -> {
-                switch (action) {
-                    case LinkSync.ACTION_ADD: {
-                        BlockPos a = BlockPos.fromLong(buf.readLong());
-                        BlockPos b = BlockPos.fromLong(buf.readLong());
-                        LinkData.clientAddLink(a, b);
-                        break;
+
+            // Читаем все данные из буфера СЕЙЧАС, пока он не освобождён
+            switch (action) {
+                case LinkSync.ACTION_ADD: {
+                    BlockPos a = BlockPos.fromLong(buf.readLong());
+                    BlockPos b = BlockPos.fromLong(buf.readLong());
+                    client.execute(() -> LinkData.clientAddLink(a, b));
+                    break;
+                }
+                case LinkSync.ACTION_REMOVE: {
+                    BlockPos pos = BlockPos.fromLong(buf.readLong());
+                    client.execute(() -> LinkData.clientRemoveLinks(pos));
+                    break;
+                }
+                case LinkSync.ACTION_CLEAR: {
+                    client.execute(LinkData::clientClearAll);
+                    break;
+                }
+                case LinkSync.ACTION_FULL_SYNC: {
+                    int count = buf.readVarInt();
+                    long[] pairs = new long[count * 2];
+                    for (int i = 0; i < count * 2; i++) {
+                        pairs[i] = buf.readLong();
                     }
-                    case LinkSync.ACTION_REMOVE: {
-                        BlockPos pos = BlockPos.fromLong(buf.readLong());
-                        LinkData.clientRemoveLinks(pos);
-                        break;
-                    }
-                    case LinkSync.ACTION_CLEAR: {
+                    client.execute(() -> {
                         LinkData.clientClearAll();
-                        break;
-                    }
-                    case LinkSync.ACTION_FULL_SYNC: {
-                        LinkData.clientClearAll();
-                        int count = buf.readVarInt();
                         for (int i = 0; i < count; i++) {
-                            BlockPos a = BlockPos.fromLong(buf.readLong());
-                            BlockPos b = BlockPos.fromLong(buf.readLong());
+                            BlockPos a = BlockPos.fromLong(pairs[i * 2]);
+                            BlockPos b = BlockPos.fromLong(pairs[i * 2 + 1]);
                             LinkData.clientAddLink(a, b);
                         }
-                        break;
-                    }
+                    });
+                    break;
                 }
-            });
+            }
         });
     }
 }
