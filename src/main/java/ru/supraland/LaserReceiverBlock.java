ca641ddff50d8e4b28092272ec400df4b76907cc
porolowka <barashka197@mail.ru>
2026-09-19 20:16:19 +0700

    Update LaserReceiverBlock.java

--- src/main/java/ru/supraland/LaserReceiverBlock.java
+++ src/main/java/ru/supraland/LaserReceiverBlock.java
@@ -1,31 +1,45 @@
 package ru.supraland;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.state.StateManager;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.StateManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class LaserReceiverBlock extends BlockWithEntity {
-    public static final BooleanProperty ACTIVATED = BooleanProperty.of("activated");
+    public static final BooleanProperty ACTIVE = BooleanProperty.of("active");
 
-    protected LaserReceiverBlock(Settings settings) {
+    public LaserReceiverBlock(Settings settings) {
         super(settings);
-        setDefaultState(getDefaultState().with(ACTIVATED, false));
+        setDefaultState(getDefaultState().with(ACTIVE, false));
     }
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(ACTIVATED);
+    protected void appendProperties(StateManager.Builder<net.minecraft.block.Block, BlockState> builder) {
+        builder.add(ACTIVE);
     }
 
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new LaserReceiverBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        if (type != SupralandMod.LASER_RECEIVER_BE) return null;
+        return (w, p, s, be) -> LaserReceiverBlockEntity.tick(w, p, s, (LaserReceiverBlockEntity) be);
     }
 }
